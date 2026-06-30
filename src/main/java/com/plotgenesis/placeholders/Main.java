4dfae89b69326d6ed740f8fec373d46dbb9c8c08
yoyoyo18 <coolsun706@gmail.com>
2026-07-01 05:07:48 +0800

    Update Main.java

--- src/main/java/com/plotgenesis/placeholders/Main.java
+++ src/main/java/com/plotgenesis/placeholders/Main.java
@@ -2,14 +2,16 @@ package com.plotgenesis.placeholders;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlotGenPlaceholder extends JavaPlugin {
+public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         new GensPlaceholder(this).register();
+        getLogger().info("PlotGenesisPlaceholders enabled");
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("PlotGenesisPlaceholders disabled");
     }
 }
