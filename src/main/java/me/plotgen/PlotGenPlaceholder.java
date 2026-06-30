01496db18aafa97f67801bd15c71baa39b225114
yoyoyo18 <coolsun706@gmail.com>
2026-07-01 05:06:54 +0800

    Update PlotGenPlaceholder.java

--- src/main/java/me/plotgen/PlotGenPlaceholder.java
+++ src/main/java/me/plotgen/PlotGenPlaceholder.java
@@ -1,32 +1,11 @@
-package me.plotgen;
+package com.plotgenesis.placeholders;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlotGenPlaceholder extends JavaPlugin {
 
-    private static PlotGenPlaceholder instance;
-
     @Override
     public void onEnable() {
-        instance = this;
-
-        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") == null) {
-            getLogger().severe("PlaceholderAPI not found! Disabling plugin...");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-
         new GensPlaceholder(this).register();
-
-        getLogger().info("PlotGenPlaceholder enabled successfully!");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("PlotGenPlaceholder disabled.");
-    }
-
-    public static PlotGenPlaceholder getInstance() {
-        return instance;
     }
 }
